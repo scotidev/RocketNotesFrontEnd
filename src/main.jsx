191af089b65ff2bb88113170b2a09833e9ea2b08
gabriel <gabrielscotio@gmail.com>
2023-12-30 17:58:05 -0300

    basic theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Details } from './pages/Details'
+import { ThemeProvider } from 'styled-components'
+import  theme  from "./styles/theme.js"
+import GlobalStyles from "./styles/global.js"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
